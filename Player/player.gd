1239b6fa6cb48ae87a1afd730bb33d35a7cd6a28
Glomen <nasso@mail.ru>
2023-10-31 02:59:29 +0300

    Add Random Coin on canvas

--- Player/player.gd
+++ Player/player.gd
@@ -9,6 +9,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var anim = $AnimatedSprite2D
 var health = 100
+var gold = 0
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -46,3 +47,7 @@ func _physics_process(delta):
 		get_tree().change_scene_to_file("res://menu.tscn")
 		
 	move_and_slide()
+
+
+func _on_timer_timeout():
+	pass # Replace with function body.
